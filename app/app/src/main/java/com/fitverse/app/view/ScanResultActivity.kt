becba04fee8da8ba4751a3aa578c590e95802dea
andikawidiantr <andikawidiantara.11@gmail.com>
2022-05-17 00:19:16 +0800

    scan result

---
+++ app/app/src/main/java/com/fitverse/app/view/ScanResultActivity.kt
@@ -0,0 +1,12 @@
+package com.fitverse.app.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fitverse.app.R
+
+class ScanResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan_result)
+    }
+}
