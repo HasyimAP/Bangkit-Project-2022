9703993cabc28d89aa70581195e4e8e8f17af3bd
Gungde Iswara <gungde120@gmail.com>
2022-06-01 16:44:32 +0800

    Add Food scanning feature

--- app/app/src/main/java/com/fitverse/app/view/scanResult/ScanResultActivity.kt
+++ app/app/src/main/java/com/fitverse/app/view/scanResult/ScanResultActivity.kt
@@ -1,12 +1,34 @@
 package com.fitverse.app.view.scanResult
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.fitverse.app.R
+import com.fitverse.app.databinding.ActivityScanResultBinding
+import com.fitverse.app.view.food.ScanFoodActivity
 
 class ScanResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityScanResultBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scan_result)
+        binding = ActivityScanResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        //Testing
+        binding.apply {
+            resultName.text = intent.getStringExtra("result")
+            listAkurasi.text = intent.getStringExtra("list_akurasi")
+
+            rescanButton.setOnClickListener{
+                onBackPressed()
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
